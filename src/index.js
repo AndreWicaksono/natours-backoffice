d19af9ecd13921eade2f9c6f8bffd856aaa57477
Andre Wicaksono <andrewicaksana@gmail.com>
2022-03-19 22:39:22 +0700

    Added Tour collection & toursWishlist Query for Authenticated User

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+("use strict");
 
 module.exports = {
   /**
@@ -7,7 +7,60 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const { transformArgs } = strapi
+      .plugin("graphql")
+      .service("builders").utils;
+    const { toEntityResponseCollection } = strapi
+      .plugin("graphql")
+      .service("format").returnTypes;
+    const extensionService = strapi.plugin("graphql").service("extension");
+
+    extensionService.use(({ nexus }) => ({
+      // Add a relation field into user https://github.com/strapi/strapi/issues/12016
+
+      types: [
+        nexus.extendType({
+          type: "Query",
+          definition(t) {
+            t.field("toursWishlist", {
+              args: {
+                pagination: "PaginationArg",
+                sort: nexus.stringArg("Order by"),
+              },
+              type: "TourEntityResponseCollection",
+              resolve: async (root, args, context) => {
+                const transformedArgs = transformArgs(args, {
+                  contentType: strapi.contentTypes["api::tour.tour"],
+                  usePagination: true,
+                });
+
+                const [toursEntries, toursCount] = await strapi.db
+                  .query("api::tour.tour")
+                  .findWithCount({
+                    // uid syntax: 'api::api-name.content-type-name'
+                    where: {
+                      users: {
+                        id: {
+                          $eq: context.state.user.id,
+                        },
+                      },
+                    },
+                    offset: args?.pagination?.start,
+                    limit: args?.pagination?.limit,
+                  });
+
+                return toEntityResponseCollection(toursEntries ?? [], {
+                  args: transformedArgs,
+                  resourceUID: "api::tour.tour",
+                });
+              },
+            });
+          },
+        }),
+      ],
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
