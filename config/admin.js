ae75ae22c47cbb281ada3224be275a64b6e85a1d
Andre Wicaksono <andre@rukita.co>
2022-12-18 01:22:46 +0700

    Added missing apiToken.salt within admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '7087fcbc4177daf3b5dc3601bdc89206'),
+    secret: env("ADMIN_JWT_SECRET", "7087fcbc4177daf3b5dc3601bdc89206"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
